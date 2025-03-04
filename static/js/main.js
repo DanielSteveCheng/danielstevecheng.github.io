69a0ee824a77386e0d00f9fb77a769530b5d3909
DanielSteveCheng <daniel.cheng@msn.com>
2025-03-04 00:12:58 -0500

    Begin adding experience

--- static/js/main.js
+++ static/js/main.js
@@ -31,6 +31,50 @@ sections.forEach(section => {
     observer.observe(section);
 });
 
+const buttonRight = document.getElementById('slideRight');
+const buttonLeft = document.getElementById('slideLeft');
+
+buttonRight.onclick = function () {
+    document.getElementById('experiences').scrollLeft += 20;
+};
+buttonLeft.onclick = function () {
+    document.getElementById('gallery').scrollLeft -= 20;
+};
+
+//for experience
+
+// const experiences = document.querySelectorAll(".gallery li");
+
+// const removeActiveExperience = () => {
+//     experiences.forEach(experience => {
+//         experience.classList.remove("active");
+//     })
+// };
+
+// const addActiveExperience = (entries, observer) => {
+//     entries.forEach(entry => {
+//         if(entry.isIntersecting) {
+//             console.log(entry.target.id);
+//             let currentEx = document.querySelector(
+//                 `.gallery li[id="${entry.target.id}"]`);
+//             removeActiveExperience();
+//             currentEx.classList.add("active");
+//         }
+//     })
+// };
+
+// const exOptions = {
+//     threshold: 0.8, 
+// };
+
+// const exObserver = new IntersectionObserver(addActiveExperience, exOptions);
+
+// const exEntries = document.querySelectorAll("li");
+
+// exEntries.forEach(section => {
+//     exObserver.observe(section);
+// });
+
 // for horizontal scroll
 
 // const init = function(){
